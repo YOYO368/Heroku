694232169e0f590ce36d719dcf862162a72b4772
YOYO368 <yoyo368@ess.ais.ac.nz>
2019-08-13 17:37:41 +1200

    Update database.php

--- database.php
+++ database.php
@@ -2,8 +2,8 @@
 	session_start();
 	
 	$host = "us-cdbr-iron-east-02.cleardb.net";
-	$database = "heroku_1bd2b709751bb09";
-	$user = "ba0c132d7fde44";
-	$password = "667e3d22";
+	$database = "heroku_7a5e9375a8d8807";
+	$user = "bab47069dbc174";
+	$password = "1e5dc72f";
 
 	$connection = mysqli_connect($host, $user, $password, $database);
